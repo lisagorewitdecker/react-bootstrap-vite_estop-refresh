d822bc3e3669e1734ebc4163a827d740764d11c3
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2026-06-10 16:23:20 -0700

    Add files via upload
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    "port": 8080,
-  }
-})
+    plugins: [react()],
+    server: {
+        cors: {
+            origin: ['http://localhost:8080', 'https://www.estopdrivingschool.com'],
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            credentials: true
+        }
+    }
+});
